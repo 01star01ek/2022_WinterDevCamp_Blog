f04d58bbf1515aa8ab22146d5c309e1cae42d44e
01star01ek <01star01ek@gmail.com>
2022-12-21 13:38:55 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
+
+  let [글제목,b] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [num, c] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick = {() => {
+          let copy = [...글제목];
+          copy[0] = '여자 코트 추천';
+          b(copy);}}>버튼</button>
+
+        <h4 style = {{color:'black', fontSize : '16px'}}>{글제목[0]}<span onClick={() => {c(num + 1)}}>😍😍</span>{num}</h4>
+        <p>2월 17일 발행</p>
+        <hr></hr>
+        <h4 style = {{color:'black', fontSize : '16px'}}>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+        <hr></hr>
+        <h4 style = {{color:'black', fontSize : '16px'}}>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+        <hr></hr>
+        
+          <Compose></Compose>
+
+      </div>
     </div>
   );
+}
+
+function Compose() {
+  return(
+  <div className = '설명'>
+    <h4>제목</h4>
+    <p>날짜</p>
+    <p>상세내용</p>
+  </div>);
 }
 
 export default App;
