f505a9e956aeff3f36cc5e11e783f4a685f399c1
01star01ek <01star01ek@gmail.com>
2022-12-22 18:29:18 +0900

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   const[title, setTitle] = useState('');
@@ -11,21 +17,34 @@ function App() {
     })
   }
   return (
-      <div className="container">
-        <div className="mb-3">
-        <label>
-          <label className="form-label">Title</label>
-          <textarea className="form-control" vlaue={title} onChange = {(event) =>  {setTitle(event.target.value)}}/>
-        </label>
-        </div>
-        <div className="mb-3">
-        <label>
-          <label className="form-label">Body</label>
-          <textarea rows={20} className="form-control" vlaue={body} onChange = {(event) =>  {setBody(event.target.value)}}/>
-        </label>
-        </div>
-        <button className="btn btn-outline-danger" onClick={onSubmit}>포스팅</button>
+    <Router>
+      <div>
+        <Link to ="/">Home</Link>
+        <Link to = "/blogs">Blogs</Link>
       </div>
+      <Switch>
+        <Route path ="/">
+          Home page
+        </Route>
+        <Route path ="/blogs">
+            <div className="container">
+            <div className="mb-3">
+            <label>
+              <label className="form-label">Title</label>
+              <textarea className="form-control" vlaue={title} onChange = {(event) =>  {setTitle(event.target.value)}}/>
+            </label>
+            </div>
+            <div className="mb-3">
+            <label>
+              <label className="form-label">Body</label>
+              <textarea rows={20} className="form-control" vlaue={body} onChange = {(event) =>  {setBody(event.target.value)}}/>
+            </label>
+            </div>
+            <button className="btn btn-outline-danger" onClick={onSubmit}>포스팅</button>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
