cb0cbef387b2482907c2415306fe8fe61a03387a
01star01ek <01star01ek@gmail.com>
2022-12-22 15:14:00 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from 'react';
-
+import { useState } from "react";
 
 function App() {
 
-  let [글제목,b] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
-  let [num, c] = useState(0);
-
+  
   return (
-    <div className="App">
-      <div>
-        <button onClick = {() => {
-          let copy = [...글제목];
-          copy[0] = '여자 코트 추천';
-          b(copy);}}>버튼</button>
-
-        <h4 style = {{color:'black', fontSize : '16px'}}>{글제목[0]}<span onClick={() => {c(num + 1)}}>😍😍</span>{num}</h4>
-        <p>2월 17일 발행함</p>
-        <hr></hr>
-        <h4 style = {{color:'black', fontSize : '16px'}}>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-        <hr></hr>
-        <h4 style = {{color:'black', fontSize : '16px'}}>{글제목[2]}</h4>
-        <p>2월 17일 발행</p>
-        <hr></hr>
-        
-          <Compose></Compose>
-
+      <div className="container">
+        <div>
+        <label>
+          <label className="form-label">Title</label>
+          <input className="form-control"/>
+        </label>
+        </div>
+        <button className="btn btn-outline-danger">포스팅</button>
       </div>
-    </div>
   );
-}
-
-function Compose() {
-  return(
-  <div className = '설명'>
-    <h4>제목</h4>
-    <p>날짜</p>
-    <p>상세내용</p>
-  </div>);
 }
 
 export default App;
