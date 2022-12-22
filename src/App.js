52d6c3f7533300d9fe31867d05bd1ca04e64b61a
01star01ek <01star01ek@gmail.com>
2022-12-22 17:49:29 +0900

    third

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import { useState } from "react";
+import axios from 'axios';
 
 function App() {
-
-  
+  const[title, setTitle] = useState('');
+  const[body, setBody] = useState('');
+  const onSubmit = () => {
+    axios.post('http://localhost:3001/posts', {
+      title,
+      body
+    })
+  }
   return (
       <div className="container">
-        <div>
+        <div className="mb-3">
         <label>
           <label className="form-label">Title</label>
-          <input className="form-control"/>
+          <textarea className="form-control" vlaue={title} onChange = {(event) =>  {setTitle(event.target.value)}}/>
         </label>
         </div>
-        <button className="btn btn-outline-danger">포스팅</button>
+        <div className="mb-3">
+        <label>
+          <label className="form-label">Body</label>
+          <textarea rows={20} className="form-control" vlaue={body} onChange = {(event) =>  {setBody(event.target.value)}}/>
+        </label>
+        </div>
+        <button className="btn btn-outline-danger" onClick={onSubmit}>포스팅</button>
       </div>
   );
 }
